866ea19950a30d87f77a9698d5334b75f034145f
Mela-nina <oge4sym@gmail.com>
2023-02-17 12:15:27 -0800

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,19 @@
  * main - prints all combination of single digits
  * Return: Always 0
  */
+
 int main(void)
-{
-	int a;
+{	int a;
 
 	for (a = 0; a < 10; a++)
-
+	{
 		putchar(a + '0');
 		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
