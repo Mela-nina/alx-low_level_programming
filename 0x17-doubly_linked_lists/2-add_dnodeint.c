8e6bf1cee2c9fea8aaa53648d80c07f71766caba
Mela-nina <oge4sym@gmail.com>
2023-06-15 08:25:21 -0700

    Correctiong for task 2-4

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,20 +3,23 @@
 /**
  * add_dnodeint - This adds new node at the beginning of dlistint_t list
  * @head: This a pointer to the head of dlistint_t list
- * @m: This is integer for new node to contain
+ * @n: This is data to insert new node
  * Return: Null - If the function fails
  *         Otherwise, the address of the new node
  */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int m)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->m = m;
+	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 	if (*head != NULL)
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,11 +5,11 @@
 /**
  * add_dnodeint_end - This adds a node to the end of linked list
  * @head: This is the double pointer to beginning of the linked list
- * @m: This is the value to add to new node
+ * @n: This is the value to add to new node
  * Return: pointer to the new node, or NULL on failure
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int m)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *tmp;
 
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int m)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->m = m;
+	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
 	{
